40df63a8ebf84c7dd040a0e7d44247c1bc90e017
Parker Sipes <parker@parkersipes.com>
2019-01-07 17:45:33 -0500

    adding files to github

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+	<nav class="navbar navbar-expanded-sm navbar-dark bg-info mb-2">
+		<div class="container">
+		<a href="#" class="navbar-brand">My Articles</a>
+		</div>
+
+	</nav>
+
+
+
+</template>
